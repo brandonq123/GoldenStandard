b28c796457de57218151ed59a638700600602299
Claude Belizaire <139932571+Claudesaul@users.noreply.github.com>
2025-04-12 16:17:25 -0400

    Added frontend and backend folders for organization

---
+++ Backend/fastAPI.py
@@ -0,0 +1,27 @@
+# Create FastAPI app
+
+## Setup logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+# Load environment variables
+load_dotenv()
+
+# Create FastAPI app
+app = FastAPI(title="Sentiment App API")
+
+# Setup CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
+)
+
+# Initial Routes
--- ExampleFastAPI.py
+++ ExampleFastAPI.py
@@ -1,21 +1,10 @@
 import os
-import jwt
 import logging
 from typing import List, Dict, Optional
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Depends, Response
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPBearer
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, JSON, desc
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from datetime import datetime, timezone
 import json
-import anthropic
-import io
-from PyPDF2 import PdfReader
-import base64
 
 
 # Setup logging
@@ -31,7 +20,7 @@ load_dotenv()
 # Database configuration
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
 
 # Create FastAPI app
 app = FastAPI(title="Class Management API")
