4bb2a80e921ba4a6a72a7af2c0cdb66e2fdc48f2
Sohan0444 <sohanshetty2023@gmail.com>
2025-04-12 19:12:08 -0400

    new gemini py

--- Backend/geminiFunc.py
+++ Backend/geminiFunc.py
@@ -1,8 +1,11 @@
 from google import generativeai as genai
+from dotenv import load_dotenv
+import os
 
-genai.configure(api_key="AIzaSyBt1iE-8tZgkne451ozgjUva4FOVBSY6ug")
+load_dotenv(dotenv_path="env/.env")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Who is the best nba player of all time?")
 print(response.text)
 
-def generateSentimentAnylsis(prompt: str):
+# def generateSentimentAnylsis(prompt: str):
