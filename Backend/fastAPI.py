4cb99444c99d328f0c121035cfec8f7b6385d708
Claude Belizaire <139932571+Claudesaul@users.noreply.github.com>
2025-04-12 17:56:03 -0400

    Current State

--- Backend/fastAPI.py
+++ Backend/fastAPI.py
@@ -1,4 +1,8 @@
 # Create FastAPI app
+import logging 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
 ## Setup logging
 logging.basicConfig(
